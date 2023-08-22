29c001e2e8fcd8f8bab52a5270cc337ea9e7276b
Asif Khan <asifkhan.github@gmail.com>
2023-08-22 12:25:06 +0600

    model created for survey response

---
+++ backend/app/Models/SurveyResponse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyResponse extends Model {
+    use HasFactory;
+
+    protected $table = "responses";
+
+    protected $fillable = ['survey_id', 'responses'];
+}
