7193b42b68bed203514045dbb83c4383f0451391
Asif Khan <asifkhan.github@gmail.com>
2023-08-22 11:30:34 +0600

    migration modified for users

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,10 +17,18 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('api_token')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // create default admin while migrating
+        DB::table('users')->insert([
+            'name' => 'Tanvir Ahmed Khan',
+            'email' => 'tanvir@gmail.com',
+            'password' => bcrypt('tanvir')
+        ]);
     }
 
     /**
