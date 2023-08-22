73b7e6f3de4c18a4ffcfa038e24e4992f6b62879
Asif Khan <asifkhan.github@gmail.com>
2023-08-22 12:40:58 +0600

    layout created

---
+++ web/src/layout/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-slate-100 h-screen overflow-y-scroll">
+    <AppNavBar />
+    <slot />
+  </div>
+</template>
+
+<script>
+    import AppNavBar from '../components/AppNavBar.vue'
+
+    export default {
+        created(){
+            document.title = "Login"
+        },
+        components: {
+            AppNavBar
+        }
+    }
+</script>
