b6bf4ab27931cd841df3e72f824eb91d62263143
Asif Khan <asifkhan.github@gmail.com>
2023-08-22 12:54:53 +0600

    response json added

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status'=> 200,
+        'Project'=>'API Backend for Survey App',        
+        'Programming Language'=>'PHP ' . phpversion(),
+        'Framework'=>'Laravel ' . app()->version()
+    ]);
 });
