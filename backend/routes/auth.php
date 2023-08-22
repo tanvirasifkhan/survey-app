9b649602149959dcc1d9b4be15cf32ee313485ad
Asif Khan <asifkhan.github@gmail.com>
2023-08-22 11:25:54 +0600

    auth routes created

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -3,3 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
